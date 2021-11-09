c9871ede46062db51bd305ed8af93836af680f5c
Ruslan Aleyev <aleyev.rus@gmail.com>
2021-11-10 00:13:22 +0300

    CreatedListsProject

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Lists;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(5);
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(5);
+            arrayList.Add(1);
+            arrayList.Add(2);
+            arrayList.Add(3);
+            arrayList.Add(4);
+            arrayList.Add(5);
+
+            arrayList.WriteToConsole();
+        }
+    }
+}
---
+++ Lists/ArrayList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Lists
+{
+    public class ArrayList
+    {
+        public int Length {get; private set;}
+
+        private const int _minArrayLenght = 10;
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+
+            Length = 0;
+            _array = new int[_minArrayLenght];
+        }
+        
+        public void Add(int value)
+        {
+            if (Length == _array.Length)
+            {
+                UpArraySize();
+            }
+
+            _array[Length] = value;
+            Length++;
+        }
+
+        public void WriteToConsole()
+        {
+            for(int i=0; i< Length; i++)
+            {
+                Console.Write($"{_array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    
+        private void UpArraySize()
+        {
+            int[] tmpArray = new int[(int)(Length * 1.5)];
+            for (int i = 0; i < Length; i++)
+            {
+                tmpArray[i] = _array[i];
+            }
+            _array = tmpArray;
+        }
+
+        public void Add(int value)
+        {
+            value = _array[0];
+        }
+
+            private void MoveElementsForward()
+        {
+            Length++;
+            for (int i = 0; i < Length; i++)
+            {
+                tmpArray[i] = _array[i + 1];
+            }
+            
+        }
+    }
+}
