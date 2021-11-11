c0480292ecf74ba0a9b4ba04a5f6c00df50a49d3
Ruslan Aleyev <aleyev.rus@gmail.com>
2021-11-12 00:47:58 +0300

    AddFirstTestAdded

--- Lists.Test/ArrayListsTests.cs
+++ Lists.Test/ArrayListsTests.cs
@@ -17,5 +17,15 @@ namespace Lists.Test
             actual.AddElement(value);
             Assert.AreEqual(expected, actual);
         }
+        [TestCase(2, new int[] { 5, 4, 6, 7 }, new int[] {2, 5, 4, 6, 7})]
+        [TestCase(0, new int[] { }, new int[] {0})]
+        [TestCase(-2, new int[] { 0 }, new int[] {-2, 0})]
+        public void AddFirstTest(int value, int[] Array1, int[] Array2)
+        {
+            ArrayList actual = new ArrayList(Array1);
+            ArrayList expected = new ArrayList(Array2);
+            actual.AddFirst(value);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
