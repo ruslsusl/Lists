c6cedce1cae27012ccca80c5c88e4a670e75e681
Ruslan Aleyev <aleyev.rus@gmail.com>
2021-11-13 01:15:48 +0300

    DeleteElementsTestsAdded

--- Lists.Test/ArrayListsTests.cs
+++ Lists.Test/ArrayListsTests.cs
@@ -37,5 +37,36 @@ namespace Lists.Test
             actual.AddElement(index, value);
             Assert.AreEqual(expected, actual);
         }
+        [TestCase(new int[] { 5, 4, 6, 7 }, new int[] { 5, 4, 6})]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 0 }, new int[] { })]
+        public void DeleteLastTest(int[] Array1, int[] Array2)
+        {
+            ArrayList actual = new ArrayList(Array1);
+            ArrayList expected = new ArrayList(Array2);
+            actual.DeleteLast();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(new int[] { 5, 4, 6, 7 }, new int[] { 4, 6, 7 })]
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 0 }, new int[] { })]
+        public void DeleteFirstTest(int[] Array1, int[] Array2)
+        {
+            ArrayList actual = new ArrayList(Array1);
+            ArrayList expected = new ArrayList(Array2);
+            actual.DeleteFirst();
+            Assert.AreEqual(expected, actual);
+        }
+        [TestCase(2, new int[] { 5, 4, 6, 7 }, new int[] { 5, 4, 7 })]
+        [TestCase(0, new int[] {1}, new int[] { })]
+        [TestCase(5, new int[] { }, new int[] { })]
+        public void DeleteElementTest(int index, int[] Array1, int[] Array2)
+        {
+            ArrayList actual = new ArrayList(Array1);
+            ArrayList expected = new ArrayList(Array2);
+            actual.DeleteElement(index);
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
